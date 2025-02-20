7d989ae462d0e48e71a99986daf80842d0758435
totchi1510 <179804262+totchi1510@users.noreply.github.com>
2025-02-20 15:28:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = 'AIzaSyAYrVuvORhSMtfBahNHyvsYsZ_LoGtPb-Q';  // Replace with your API Key
+const apiKey = 'AIzaSyCnKO4VX7mzk8BNZv1ItWFVpSMHxUKoU4g';  // Replace with your API Key
 const spreadsheetId = '1Y9UWQbNmzGvPZxVTs732iUHo9o26KJDbSumj08xObjs';  // Replace with your Spreadsheet ID
 const range = 'Sheet1!A1:B100';  // Adjust the range to your sheet (dates in column A, values in column B)
 
