0a9e885b37d22077d4291760202fb2926943413d
totchi1510 <179804262+totchi1510@users.noreply.github.com>
2025-02-20 17:24:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function displayData(data) {
 
 // Register the service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('service-worker.js')
     .then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
     })
