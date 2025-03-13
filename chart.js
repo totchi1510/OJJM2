5d0c093aa188aa623560ed0c031292010e82c480
totchi1510 <179804262+totchi1510@users.noreply.github.com>
2025-03-13 11:50:43 +0900

    Update chart.js

--- chart.js
+++ chart.js
@@ -54,7 +54,11 @@ function drawChart(data) {
           grid: {
             drawOnChartArea: false  // 右軸のグリッドラインを消す
           }
-        }
+          ticks:{
+            min:0,
+            max:100,
+            stepSize:5,
+          }
       }
     }
   });
