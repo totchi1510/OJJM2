4731e0f70baa5584a99ace20e804c8630a5104e5
totchi1510 <179804262+totchi1510@users.noreply.github.com>
2025-03-13 12:33:59 +0900

    Update chart.js

--- chart.js
+++ chart.js
@@ -14,7 +14,7 @@ function drawChart(data) {
           data: drivingScores,
           borderColor: 'rgba(75, 192, 192, 1)',  // Line color for driving scores
           fill: false,
-          yAxisID: 'y-axis-main'  // 統一Y軸
+          yAxisID: 'y-axis-driving'  // 統一Y軸
         },
         {
           label: '心拍数',
@@ -37,14 +37,27 @@ function drawChart(data) {
         y: {
           title: {
             display: true,
-            text: 'スコア・心拍数'
+            text: '心拍数'
           },
-          id: 'y-axis-main',
+          id: 'y-axis-heart',
           position: 'left',  // 右側の軸を削除
           beginAtZero: true,
           min: 0,
           max: 100
-        }
+          },
+          y1: {
+          title: {
+            display: true,
+            text: '運転スコア'
+          },
+          id: 'y-axis-driving',
+          position: 'right',  // 運転スコアを右側に
+          beginAtZero: true,
+          min: 0,
+          max: 100,
+          grid: {
+            drawOnChartArea: false  // グリッド線が重ならないように
+          }
       }
     }
   });
